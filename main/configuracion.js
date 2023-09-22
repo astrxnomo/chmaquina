b11ecd82e52484fa7be7805d9215d75b51ea69be
astrxnomo <lgiraldoo@unal.edu.co>
2023-09-22 18:39:01 -0500

    update

---
+++ main/configuracion.js
@@ -0,0 +1,27 @@
+
+
+
+
+//Velocidad
+const sliderVelocidad = document.getElementById("velocidadSlider");
+function updateVelocidadValue(valor) {
+    document.getElementById("velocidadValue").innerHTML = valor;
+}
+const valorVelocidad = slider.value;
+
+
+//Memoria
+const sliderMemoria = document.getElementById("memoriaSlider");
+function updateMemoriaValue(valor) {
+    document.getElementById("memoriaValue").innerHTML = valor;
+}
+const valorMemoria = slider.value;
+
+
+//Kernel
+const sliderKernel = document.getElementById("kernelSlider");
+function updateKernelValue(valor) {
+    document.getElementById("kernelValue").innerHTML = valor;
+}
+const kernelVelocidad = slider.value;
+
