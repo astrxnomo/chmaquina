a12ae9e3f03ac4948b9e677f695db769dab643ea
astrxnomo <lgiraldoo@unal.edu.co>
2023-09-22 10:20:19 -0500

    update

---
+++ main/main.js
@@ -0,0 +1,19 @@
+document.getElementById('boton1').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+
+document.getElementById('boton2').addEventListener('click', function() {
+    // Aquí va la funcionalidad del Botón 2
+});
+
+document.getElementById('boton3').addEventListener('click', function() {
+    // Aquí va la funcionalidad del Botón 3
+});
+
+document.getElementById('boton4').addEventListener('click', function() {
+    // Aquí va la funcionalidad del Botón 4
+});
+
+document.getElementById('boton5').addEventListener('click', function() {
+    // Aquí va la funcionalidad del Botón 5
+});
