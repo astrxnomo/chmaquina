81a01b8c814270318fee00d496fe21cea251aed4
astrxnomo <lgiraldoo@unal.edu.co>
2023-09-23 15:36:15 -0500

    update

--- main/js/footer.js
+++ main/js/footer.js
@@ -6,23 +6,6 @@ document.getElementById('fileInput').addEventListener('change', function(e) {
     reader.onload = function(e) {
         var contents = e.target.result;
         var lines = contents.split('\n');
-        var tableBody = document.getElementById('fileTable').getElementsByTagName('tbody')[0];
-
-        // Limpiar la tabla antes de agregar nuevas líneas
-        tableBody.innerHTML = '';
-
-        // Agregar cada línea a la tabla
-        for (var i = 0; i < lines.length; i++) {
-            var newRow = tableBody.insertRow();
-            var idCell = newRow.insertCell(0);
-            var contentCell = newRow.insertCell(1);
-
-            // Asegúrate de que el ID siempre tenga 4 dígitos
-            var id = String(i + 1).padStart(4, '0');
-
-            idCell.textContent = id;
-            contentCell.textContent = lines[i];
-        }
 
         // Actualiza el footer con la información del archivo
         document.getElementById('fileInfo').textContent = 'Programa: ' + file.name + ' | Instrucciones: ' + lines.length;
